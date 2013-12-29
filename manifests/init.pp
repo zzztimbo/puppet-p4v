bda8caf00097610126515ce9c752ff30defc0f2c
Tim Chan <tim@chan.net>
2013-12-28 19:00:49 -0800

    passing tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,16 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install p4v to /Applications
+#
+# Sample Usage:
+#
+#  class { 'p4v' :
+#    version => 'r13.4'
+#  }
+#
+
+class p4v($version='r13.4')
+{
+  package { 'P4V.dmg':
+    provider => 'appdmg_eula',
+      source => "http://filehost.perforce.com/perforce/${version}/bin.macosx106x86_64/P4V.dmg"
+  }
 }
